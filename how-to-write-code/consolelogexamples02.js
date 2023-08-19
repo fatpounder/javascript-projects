d264397ad83e0a7be0180175bdc4e6f7e981f86e
Marie Joiner <marie5joiner@gmail.com>
2023-08-19 14:39:09 -0500

    ConsoleLogExample2 Commit

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,5 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+console.log("This is my practice text, here comes the next line \n here it is and now the space \t yep")
