7906cc387dfe6eec2effd13fd55e6b8c5894c079
Marie Joiner <marie5joiner@gmail.com>
2023-10-09 20:22:11 -0500

    studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,37 @@
 
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(num) {
+    let result = ""
+    if (num / 2 == 1) {
+      return "Launch!";
+    }
+    if (num / 3 == 1) {
+      return  "Code!";
+    }
+    if (num / 5 == 1) {
+      if (result.length > 0) {
+        result += " "
+      }
+      return "Rocks!";
+    }
+    if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+      return "LaunchCode Rocks!"
+    } else if (num % 2 === 0 && num % 3 === 0) {
+        return "LaunchCode!"
+    } else if (num % 3 === 0 && num % 5 === 0) {
+        return "Code Rocks!"
+    } else if  (num % 2 === 0 && num % 5 === 0) {
+        return "Launch Rocks! (CRASH!!!!)"
+    }
+    if (num % 2 !== 0 && num % 3 !== 0 && num % 5 !== 0) {
+      return "Rutabagas! That doesn't work."
+    }
+  }
+};
 
 module.exports = launchcode;
 
