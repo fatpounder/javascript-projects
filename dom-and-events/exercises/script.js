93c07a9de8a002f3c306d8235305139512d03490
marie@FatPounder <marie5joiner@gmail.com>
2023-10-29 22:56:08 -0500

    dom-and-events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,9 +2,20 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
-
-    // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    })
+    missionAbort.addEventListener("mouseover", event => {
+        event.target.style.backgroundColor = "#ff0000";
+    })
+    missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+    })
+    missionAbort.addEventListener("click", function() {
+        if (window.confirm("Are you sure you want to abort the mission?")) {
+            window.confirm("Mission aborted! Space shuttle returning home")
+        }
+    })
 }
 
 window.addEventListener("load", init);
